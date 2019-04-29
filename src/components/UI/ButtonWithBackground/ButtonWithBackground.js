3451a615b88c140396c706c15f354cbf1a8dc68e
Sithembiso <isitheh@gmail.com>
2019-04-29 23:18:36 +0200

    React Native Animations and handling user input.

--- src/components/UI/ButtonWithBackground/ButtonWithBackground.js
+++ src/components/UI/ButtonWithBackground/ButtonWithBackground.js
@@ -3,14 +3,29 @@ import { TouchableOpacity, TouchableNativeFeedback, Text, View, StyleSheet, Plat
 
 const buttonWithBackground = props => {
   const content = (
-    <View style={[styles.button, { backgroundColor: props.color }]}>
-      <Text>{props.children}</Text>
+    <View style={[
+      styles.button,
+      { backgroundColor: props.color },
+      props.disabled ? styles.disabled : null]}>
+      <Text style={props.disabled ? styles.disabledText: null}>
+        {props.children}
+      </Text>
     </View>
   );
-  if (Platform.OS === 'android') {
-    return (<TouchableNativeFeedback onPress={props.onPress}>{ content }</TouchableNativeFeedback>);
+  if(props.disabled) {
+    return content;
   }
-  return (<TouchableOpacity onPress={props.onPress}>{ content }</TouchableOpacity>);
+  if (Platform.OS === 'android') {
+    return (
+      <TouchableNativeFeedback onPress={props.onPress}>
+        { content }
+      </TouchableNativeFeedback>
+    );
+  }
+  return (
+    <TouchableOpacity onPress={props.onPress}>
+      { content }
+    </TouchableOpacity>);
 };
 
 const styles = StyleSheet.create({
@@ -20,6 +35,13 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     borderWidth: 1,
     borderColor: "black"
+  },
+  disabled: {
+    backgroundColor: "#eee",
+    borderColor: "#aaa"
+  },
+  disabledText: {
+    color: "#aaa"
   }
 });
 
